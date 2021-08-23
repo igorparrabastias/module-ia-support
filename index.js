312dfecaf2879d7c24128d19e2ae0a8513a756ae
Igor Parra <usuario3@gmail.com>
2021-08-23 14:32:30 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const migrator = require('./lib/migrator')
+const helpers = require('./lib/helpers')
+const categoriesIds = require('./lib/categoriesIds')
+
+module.exports = {
+  migrator,
+  helpers,
+  categoriesIds
+}
